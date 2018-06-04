a95bd3c27512dded112b0da00eaa7b240bcce408
Timothy Stott <stott.timothy@gmail.com>
2018-06-04 22:35:11 +0100

    [zsh] load rbenv without rehash

--- zsh/.zconfig/ruby.zsh
+++ zsh/.zconfig/ruby.zsh
@@ -1,5 +1,5 @@
-# Load ruby version manager
-[[ -s "$HOME/.rbenv" ]] && eval "$(rbenv init -)"
+# Load ruby version manager CLI without rehashing (slow)
+[[ -s "$HOME/.rbenv" ]] && eval "$(rbenv init --no-rehash -)"
 
 alias be='bundle exec'
 alias zs='zeus start'
