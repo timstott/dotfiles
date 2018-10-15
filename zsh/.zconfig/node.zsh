46e9491235f27c2ebcdae7e6004924564f934e62
Timothy Stott <stott.timothy@gmail.com>
2018-10-15 18:22:47 +0100

    [shell] switch ruby and node version managers

--- zsh/.zconfig/node.zsh
+++ zsh/.zconfig/node.zsh
@@ -1,9 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
-
-# Load node version manager CLI (fast) wihout loading a node version(slow)
-# thanks to `--no-use`.
-[ -s "$NVM_DIR/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh" --no-use
-
 export PATH="$HOME/.yarn/bin:$PATH"
 
 # Use npm-exec to local binaries
--- zsh/.zconfig/ruby.zsh
+++ zsh/.zconfig/ruby.zsh
@@ -1,6 +1,15 @@
-# Load ruby version manager CLI without rehashing (slow)
-[[ -s "$HOME/.rbenv" ]] && eval "$(rbenv init --no-rehash -)"
-
 alias be='bundle exec'
 alias zs='zeus start'
 alias zc='zeus console'
+
+chruby_fn=/usr/local/opt/chruby/share/chruby/chruby.sh
+chruby_auto=/usr/local/share/chruby/auto.sh
+
+if [ -f $chruby_fn ] && [ -f $chruby_auto ]; then
+  source $chruby_fn
+  source $chruby_auto
+else
+  YELLOW="\e[33m"
+  DEFAULT="\e[0m"
+  echo "${YELLOW}chruby not installed${DEFAULT}"
+fi
