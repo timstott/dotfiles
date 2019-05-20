68d9fb5224c26a5459e264a3d294eaae1904c7e0
Timothy Stott <stott.timothy@gmail.com>
2019-05-20 23:26:45 +0100

    [nixos] babylon box configuration

---
+++ hosts/babylon/configuration.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = [];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    promptInit = "";
+  };
+
+  users.users."timothy.stott" = {
+    shell = pkgs.zsh;
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 1;
+  nix.buildCores = 1;
+}
