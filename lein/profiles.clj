1d2c371efd531626404fcca86b5699314e6d3deb
Timothy Stott <stott.timothy@gmail.com>
2019-06-28 21:42:46 +0100

    [clojure] remove lein config

--- lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user {:dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]
-
-        :plugins [[refactor-nrepl "2.2.0-SNAPSHOT"]
-                  [lein-pprint "1.1.1"]]}
-
- :repl {:plugins [[cider/cider-nrepl "0.11.0"]]}}
